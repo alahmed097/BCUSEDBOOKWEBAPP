86aed9ad22855e8009f4655ca77d941d0f2f5fb4
Ali  Ahmed <AliAhmed@Alis-Air.home>
2019-10-11 17:04:52 -0400

    second commit

--- src/server.py
+++ src/server.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import time
+import cmd
 
 app = Flask(__name__)
 
@@ -6,6 +8,24 @@ app = Flask(__name__)
 def departments():
     return render_template("departments.html")
 
+
+@app.route('/<cmd>')
+def execute(cmd=None):
+    page = ''
+
+    if cmd == 'ANTH':
+        page = 'ANTH.html'
+    if cmd == 'BIO':
+      page = 'BIO.html'
+    if cmd == 'CISC':
+      page = 'CISC.html'
+    if cmd == 'ENG':
+      page = 'ENG.html'
+    if cmd == 'MATH':
+      page = 'MATH.html'
+
+    if page != '':
+      return render_template(page)
 
 
 if __name__ =="__main__":

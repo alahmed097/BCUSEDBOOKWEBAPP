c98e3056e881eed1fcd40f7df5e499795fafe041
Ali  Ahmed <AliAhmed@Alis-MacBook-Air.local>
2019-11-04 14:56:16 -0500

    Latest

--- src/server.py
+++ src/server.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
 import time
 import cmd
+import sqlite3
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def departments():
@@ -14,7 +16,7 @@ def execute(cmd=None):
     page = ''
 
     if cmd == 'ANTH':
-        page = 'ANTH.html'
+      page = 'ANTH.html'
     if cmd == 'BIO':
       page = 'BIO.html'
     if cmd == 'CISC':
@@ -26,6 +28,36 @@ def execute(cmd=None):
 
     if page != '':
       return render_template(page)
+
+
+
+
+    def database(name, data):
+        conn = sqlite3.connect("YTD.db")
+        cursor = conn.cursor()
+
+        cursor.execute("""CREATE TABLE IF NOT EXISTS my_table (name TEXT,data BLOP) """)
+        cursor.execute("""INSERT INTO my_table (name, data) VALUES (?,?) """, (name, data))
+
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+        def query():
+            conn = sqlite3.connect("YTD.db")
+            cursor = conn.cursor()
+            print("IN DATABASE FUNCTION ")
+            c = cursor.execute(""" SELECT * FROM  my_table """)
+
+            for x in c.fetchall():
+                name_v = x[0]
+                data_v = x[1]
+                break
+
+            conn.commit()
+            cursor.close()
+            conn.close()
+
 
 
 if __name__ =="__main__":
